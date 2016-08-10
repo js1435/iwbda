4519256bb68f4c1f0626341abec0b0e5f3b43d64
Vittorio Bartoli <vb15147@bristol.ac.uk>
2016-08-10 22:41:47 +0100

    Adde task1 title and finished module to stub and task delegation for task 1

--- test/seqcompare.py
+++ test/seqcompare.py
@@ -1,6 +1,15 @@
-#Module 1 - Read FASTA, div sequences (Break )
+########################################################
+# Task 1 ###############################################
+########################################################
+
+
+
+#Module 1 - Read FASTA, div sequences (Break ) ACTION J 
 #Input FASTA file 
 #Output "seq_x" where x is the sequence id 
 
-#Module 2 
-# Is a module
+#Module 2 - Length of object + compare length  - ACTION V 
+#Input "seq_x" 
+#Output "seq_x" object I.D and "seq_x " length and comparison of modules 
+
+
