4c0c1cb0bbc009f8adc883c1410b0fc0fc94b8f1
Jack Steventon <js1435@bristol.ac.uk>
2016-08-11 20:43:43 +0100

    I have added both the DNA test files and a test set of DNA sequences.
    I have also added a module which divides set fo fasta files into individual
    sequences.

--- test/seqcompare.py
+++ test/seqcompare.py
@@ -1,8 +1,25 @@
+from __future__ import print_function
+import sys
+import re
+
+file = sys.argv[1]
+
+RFILE = open( file, "r" )
+
+lines = RFILE.readlines()
+
+seqs = lines[1::2] 					#slicing lines matrix to take only the second 
+									#elements of the array i.e. the DNA seq
+
+for i in range(0, len(seqs) ):
+	seq = seqs[i].rstrip()			#removing newline comments
+	# print ( "%s" % seq )
+
+RFILE.close
+
 ########################################################
 # Task 1 ###############################################
 ########################################################
-
-
 
 #Module 1 - Read FASTA, div sequences (Break ) ACTION J 
 #Input FASTA file 
