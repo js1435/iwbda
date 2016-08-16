f767210a0d7c578fa29cfc593b578f280b38f677
Vittorio Bartoli <vb15147@bristol.ac.uk>
2016-08-16 23:23:18 +0100

    New Repressilator File for Repressilator related Eugene files

---
+++ task1/EugeneFiles/Repressilator/PropertyDefinition.h
@@ -0,0 +1,9 @@
+
+/** PROPERTIES DEFINED HERE **/
+Property Name(txt);
+Property Sequence(txt);
+Property Represses(txt);
+Property InducedBy(txt);
+Property PromoterType(txt);;
+
+Property Pigeon(txt);
