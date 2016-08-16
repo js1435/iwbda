0e446432126278c0a61a33753f5e01fe1ceccaba
Vittorio Bartoli <vb15147@bristol.ac.uk>
2016-08-16 23:19:18 +0100

    Moved Repressilator Files to specific folder

--- task1/EugeneFiles/PropertyDefinition.h
+++
@@ -1,9 +0,0 @@
-
-/** PROPERTIES DEFINED HERE **/
-Property Name(txt);
-Property Sequence(txt);
-Property Represses(txt);
-Property InducedBy(txt);
-Property PromoterType(txt);;
-
-Property Pigeon(txt);
