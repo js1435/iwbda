4cb02968ed3e47575ca170e58c092c2cd40aadc0
Vittorio Bartoli <vb15147@bristol.ac.uk>
2016-08-16 21:02:53 +0100

    Added files for Elowitz Repressilator. Files labeled with BBB at start generate an printed output of " 1 Repressilator generated" , however, txt file with output Data indicated there was an error with compilation.

---
+++ task1/EugeneFiles/PartDefinition.h
@@ -0,0 +1,10 @@
+
+/** PART TYPES ***/
+PartType SmallMolecule(Name);
+
+
+PartType Promoter(Name, Sequence, Pigeon);
+PartType RBS (Name, Sequence, Pigeon);
+PartType Repressor(Name, Sequence, Pigeon);
+PartType Reporter(Name, Sequence, Pigeon);
+PartType Terminator(Name, Sequence, Pigeon);
